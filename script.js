c63d1f12295ad4af33b4f1c6af90bc70d1fcf4c4
Wint-1234 <winton.ebanks@yahoo.com>
2022-11-01 12:25:17 -0400

    Made the main page skeleton (html)

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const bibleURL =
-    "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/search?query=Goliath&sort=relevance";
+// const bibleURL =
+//     "https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/search?query=Goliath&sort=relevance";
 
-const data5 = fetch(bibleURL, {
-    headers: {
-        accept: "application/json",
-        "api-key": "127fa726b182363266302755b5dab363",
-    },
-})
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-
-//
+// const data5 = fetch(bibleURL, {
+//     headers: {
+//         accept: "application/json",
+//         "api-key": "127fa726b182363266302755b5dab363",
+//     },
+// })
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
